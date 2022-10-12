f7116692d6d98e1f2153bc8fe6c84a751f450a5d
apem5186 <apem5186@naver.com>
2022-10-12 17:49:00 +0900

    이미지 업로드 배포 테스트

--- galleryfrontend/src/components/PostReg/upLoadForm.jsx
+++ galleryfrontend/src/components/PostReg/upLoadForm.jsx
@@ -57,7 +57,7 @@ function UpLoadForm(props) {
         }
         const formData = new FormData()
         if (!imgs === null) {
-            formData.append('files', imgs)
+            formData.append('files', images)
             console.log("이미지 " + formData.get("files").name)
         }
         formData.append('usersId', localStorage.getItem('uid'))
@@ -74,6 +74,7 @@ function UpLoadForm(props) {
                 console.log('서버')
                 console.log("이미지 : " + formData.get("files"))
                 console.log("이미지 imgs : " + imgs)
+                console.log("이미지 images : " + images)
             })
     }
 
