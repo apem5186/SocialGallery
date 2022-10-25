be98f82a351170c58fd911b582abb33696b6c1d8
apem5186 <apem5186@naver.com>
2022-10-25 20:30:48 +0900

    이미지 배포 수정 테스트

--- galleryBackend/src/main/java/com/socialgallery/gallerybackend/service/post/PostService.java
+++ galleryBackend/src/main/java/com/socialgallery/gallerybackend/service/post/PostService.java
@@ -379,14 +379,14 @@ public class PostService {
                         log.info("=======================delete File S3==============================");
                     });
                     log.info("===============files TEST=================");
-                    log.info("FILES TEST : " + !files.isEmpty() + "FILES : " + files.get(0));
+                    log.info("FILES TEST : " + !files.isEmpty() + "FILES : " + files.get(0).toString());
                     log.info("TEST 1 : " + !(files.isEmpty()));
                     log.info("===============files TEST=================");
                     s3Upload(files, post);
                 } else { // DB에 파일이 없을 경우
                     log.info("===============files TEST=================");
                     log.info("fefwe");
-                    log.info("FILES TEST : " + !files.isEmpty() + "FILES : " + files.get(0));                    log.info("TEST 1 : " + !(files.isEmpty()));
+                    log.info("FILES TEST : " + !files.isEmpty() + "FILES : " + files.get(0).toString());                    log.info("TEST 1 : " + !(files.isEmpty()));
                     log.info("TEST 1 : " + !(files.isEmpty()));
                     log.info("===============files TEST=================");
                     s3Upload(files, post);
